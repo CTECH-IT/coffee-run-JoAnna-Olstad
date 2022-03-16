764b2a4705b787c3b127933fb4ee7d1c3f23cf2a
JoAnna Olstad <5638402@isd535.org>
2022-03-16 11:10:35 -0500

    truck order

--- scripts/scripts/truck.js
+++ scripts/scripts/truck.js
@@ -3,9 +3,21 @@
 
     let App = window.App || {};
 
-    function Truck() {
+    function Truck(truckId, db) {
+        this.truckId = truckId;
+        this.db = db;
     }
 
+    Truck.prototype.createOrder = function (order) {
+        console.log('Adding order for ' + order.emailAddress);
+        this.db.add(order.emailAddress, order);
+    }
+
+    Truck.prototype.deliverOrder = function (customerId) {
+        console.log('Delivering order for ' + customerId);
+        this.db.remove(customerId);
+    }
+    
     App.Truck = Truck;
     window.App = App;
 
