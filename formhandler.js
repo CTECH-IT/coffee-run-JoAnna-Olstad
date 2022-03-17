844a6ad8e9a15caaffac8134d839368edb99c465
JoAnna Olstad <5638402@isd535.org>
2022-03-17 11:04:17 -0500

    jQuery, errors, and selector

---
+++ formhandler.js
@@ -0,0 +1,21 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App || {};
+    let $ = window.jQuery;
+
+    function FormHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided!');
+        }
+        // find the "selector" in the DOM using jQuery and assign it to this.formElement
+        this.$formElement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+    
+    App.FormHandler = FormHandler;
+    window.App = App;
+
+})(window);
