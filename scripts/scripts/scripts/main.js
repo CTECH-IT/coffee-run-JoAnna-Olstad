f8112a16b35705216d2207644744034104dcd03d
JoAnna Olstad <5638402@isd535.org>
2022-03-18 11:22:00 -0500

    Have to fix pending orders, part 4 page 14

--- scripts/scripts/scripts/main.js
+++ scripts/scripts/scripts/main.js
@@ -2,22 +2,27 @@
     'use strict';
 
     const FORM_SELECTOR = '[data-coffee-order="form"]';
-
+    const CHECKLIST_SELECTOR = '[data-coffee-order="checkList"]';
 
     // let's make sure we only have one of each of these:
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
+    let CheckList = App.CheckList;
 
     let myTruck = new Truck('12345', new DataStore());
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
 
     window.myTruck = myTruck;
 
     // find the form that is being submitted and create a formHandler object
     let formHandler = new FormHandler(FORM_SELECTOR);
-    // bind the createOrder to a specific truck and pass it to addSubmitHandler
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-    console.log(formHandler);
+    
+    // When the submit button is called, create the order and add a chackbox
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
 
 })(window);
