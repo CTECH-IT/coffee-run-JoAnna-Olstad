f9f520eec55814bae62af0544ebc8c81a5c1867e
JoAnna Olstad <5638402@isd535.org>
2022-03-22 10:43:18 -0500

    add, getAll, get, delete commit

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -11,11 +11,35 @@
 
     RemoteDataStore.prototype.add = function (key, val) {
         // Call jQuery's $.post method to send the value to the serverURL
+        // When the server responds, call an anonymous function with serverResponse
         $.post(this.serverURL, val, function (serverResponse) {
             console.log(serverResponse);
-    });
-};
+        });
 
-App.RemoteDataStore = RemoteDataStore;
-window.App = App;
-}) (window);
+        RemoteDataStore.prototype.getAll = function (cb) {
+            // make a "get" call to the server URL
+            // pass in an anonymous function that calls the "cb" callback function
+            $.get(this.serverURL, function (serverResponse) {
+                console.log(serverResponse);
+                cb(serverResponse);
+            });
+        };
+        RemoteDataStore.prototype.get = function (key, cb) {
+            // make a get call to the server, but pass an email address
+            // so that it returns just one order
+            // then call the function "cb" on the response
+            $.get(this.serverURL + '?emailAddress=' + key, function (serverResponse) {
+                console.log(serverResponse);
+                cb(serverResponse);
+            });
+        };
+    };
+
+    RemoteDataStore.prototype.remove = function (key) {
+        // call the server URL using the ajax 'DELETE' command
+        $.ajax(this.serverURL + '?emailAddress' + key, { type: 'DELETE' });
+    };
+
+    App.RemoteDataStore = RemoteDataStore;
+    window.App = App;
+})(window);
